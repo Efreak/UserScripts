ae77d1c582675c9f0936a248bf3922efcd7bc7b4
Efreak <efreak@users.noreply.github.com>
2020-03-10 14:45:24 -0700

    Add background color and icons for freeleech, make background colors transparent, alternate rows, etc

--- BakaBTImprover.user.js
+++ BakaBTImprover.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Bakabt Improver
-// @version      0.1.3
+// @version      0.1.6
 // @description  Some simple improvements for easier browsing bakabt
 // @downloadURL  https://github.com/Efreak/BakaBT-Improver/raw/master/BakaBTImprover.user.js
 // @updateURL    https://github.com/Efreak/BakaBT-Improver/raw/master/BakaBTImprover.user.js
@@ -16,7 +16,12 @@
     'use strict';
 
     var userlink = '',
-        backgroundColor = '#a7b5db',
+        setBackground = true,
+        setBakaIconOnKnown = true,
+        knownBackgroundColor = '#0029972d',
+        knownAltBackgroundColor = '#00299717',
+        freeleechBackgroundColor = '#bce0bc68',
+        freeleechAltBackgroundColor = '#bce0bc34',
         knownTorrents = [],
         updateInterval = 5, //in minutes
         $ = window.$;
@@ -25,16 +30,49 @@
         updateKnownTorrents();
     }
 
-    function markKnownTorrents() {
+    function addOwnedIcon(ele) {
+        var ico=document.createElement('span');
+        ico.className="icon owned";
+        ico.title="On your account/profile";
+        var eles=ele.querySelectorAll('td.name');
+        if(eles[0]){
+            eles[0].querySelector('a.title').insertAdjacentElement('afterend',ico)
+        }
+    }
+    function markTorrents() {
+        // remove all 'owned' icons before putting them back...too lazy to to this right
+        Array.prototype.slice.call($('span.icon.owned')).forEach(ele => {
+            ele.parentNode.removeChild(ele);
+        });
+        // mark freeleech torrents before known torrents (known don't matter)
+        Array.prototype.slice.call($('tr.torrent.alt1 td span.icon.freeleech, tr.torrent_alt td span.icon.freeleech')).forEach(function(icon){
+            if(setBackground==true) {
+                $(icon.parentNode.parentNode.querySelector('td.category')).attr('style', 'background-color:' + freeleechBackgroundColor);
+            }
+        });
+        Array.prototype.slice.call($('tr.torrent.alt0 td span.icon.freeleech, tr.torrent_alt td span.icon.freeleech')).forEach(function(icon){
+            if(setBackground==true) {
+                $(icon.parentNode.parentNode.querySelector('td.category')).attr('style', 'background-color:' + freeleechAltBackgroundColor);
+            }
+        });
+        // mark known torrents
         if(knownTorrents.length > 0) {
             var alt = null;
             for(var i = 0; i < knownTorrents.length; i++) {
                 alt = null;
-                $('tr.torrent[data-torrentid="' + knownTorrents[i] + '"]').attr('style', 'background-color:' + backgroundColor);
+                if(setBackground==true) {
+                    $('tr.torrent.alt1[data-torrentid="' + knownTorrents[i] + '"]').attr('style', 'background-color:' + knownBackgroundColor);
+                    $('tr.torrent.alt0[data-torrentid="' + knownTorrents[i] + '"]').attr('style', 'background-color:' + knownAltBackgroundColor);
+                }
+                if(setBakaIconOnKnown==true) {
+                    Array.prototype.slice.call($('tr.torrent[data-torrentid="' + knownTorrents[i] + '"]')).forEach(ele=>addOwnedIcon(ele));
+                }
                 alt = $('tr.torrent_alt[data-torrentid="' + knownTorrents[i] + '"]');
                 if($('tr.torrent_alt[data-torrentid="' + knownTorrents[i] + '"]').length) {
                     while(alt.hasClass('torrent_alt')) {
-                        alt.attr('style', 'background-color:' + backgroundColor);
+                        if(setBackground==true) {
+                            alt.attr('style', 'background-color:' + knownBackgroundColor);
+                        }
                         alt = alt.next();
                     }
                 }
@@ -44,7 +82,7 @@
 
     function updateKnownTorrents() {
         if(!needUpdate()) {
-            markKnownTorrents();
+            markTorrents();
             return;
         }
         var user = $('.username');
@@ -63,7 +101,7 @@
 
                     localStorage.setItem('BI_needUpdate', Date.now());
                     localStorage.setItem('BI_knownTorrents', JSON.stringify(knownTorrents));
-                    markKnownTorrents();
+                    markTorrents();
                 }
             });
         }
@@ -84,4 +122,8 @@
 
         return update;
     }
+    window.addEventListener('load',markTorrents);
+    var styleToAdd=document.createElement('style');
+    styleToAdd.innerHTML='span.icon.owned {\n    background:none;\n    overflow: hidden;\n    width:16px;\n    display:inline-block;\n    height:16px;\n    margin-left:2px;\n}\nspan.icon.owned:after{\n    content: url(/resources/img/favicon.png);\n}';
+    document.head.appendChild(styleToAdd);
 })();
